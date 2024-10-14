029aad5b52ad639b9598deb6ce687c436c5835da
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-10-14 13:42:06 +0900

    first commit

---
+++ src/main/java/com/sparta/spring_trello/SpringTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrelloApplication.class, args);
+    }
+
+}
