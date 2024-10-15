e1615f8624260d037454f71e93a4c701dd25f3f5
Robbie <Robbie@email.com>
2024-10-15 10:31:13 +0900

    [move] 패키지 위치 변경

--- src/main/java/com/sparta/spring_trello/domain/user/dto/SigninRequest.java
+++ src/main/java/com/sparta/spring_trello/domain/user/dto/SigninRequest.java
@@ -1,4 +1,4 @@
-package com.sparta.spring_trello.dto;
+package com.sparta.spring_trello.domain.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,8 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SignupRequest {
+public class SigninRequest {
 
     private String email;
-    private String userRole;
 }
--- src/main/java/com/sparta/spring_trello/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/spring_trello/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.sparta.spring_trello.repository;
+package com.sparta.spring_trello.domain.user.repository;
 
-import com.sparta.spring_trello.entity.User;
+import com.sparta.spring_trello.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
