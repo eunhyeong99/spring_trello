0d5e8e0ed58369dfb24595e17a6c2cd745f02e77
Robbie <Robbie@email.com>
2024-10-15 10:37:27 +0900

    [feat] comment entity 추가

--- src/main/java/com/sparta/spring_trello/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/spring_trello/domain/comment/entity/Comment.java
@@ -1,4 +1,18 @@
 package com.sparta.spring_trello.domain.comment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
+    private Long id;
+
+    private String contents;
 }
