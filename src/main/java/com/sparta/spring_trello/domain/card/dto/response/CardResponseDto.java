779a0b8d66c3f13b425f05b88d91ae1fb53b72f6
gyeol <gyeol9012@gmail.com>
2024-10-16 18:21:47 +0900

    [feat] 카드 - 리스트 연관관계 수정

--- src/main/java/com/sparta/spring_trello/domain/card/dto/response/CardResponseDto.java
+++ src/main/java/com/sparta/spring_trello/domain/card/dto/response/CardResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.spring_trello.domain.card.dto.response;
 
 import com.sparta.spring_trello.domain.card.entity.Activity;
+import com.sparta.spring_trello.domain.card.entity.Card;
 import com.sparta.spring_trello.domain.comment.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,4 +20,15 @@ public class CardResponseDto {
     private LocalDate deadline;
     private final List<Activity> activities;
     private final List<Comment> comments;
+
+
+    public CardResponseDto(Card card) {
+        this.id = card.getId();
+        this.title = card.getTitle();
+        this.contents = card.getContents();
+        this.deadline = card.getDeadline();
+        this.activities = card.getActivities() != null ? List.copyOf(card.getActivities()) : List.of();
+        this.comments = card.getComments() != null ? List.copyOf(card.getComments()) : List.of();
+
+    }
 }
